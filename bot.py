4b0203e0ffdbc50c9a1b951b28ad68d1accfa0da
Praveen G <gpraveen.ece@gmail.com>
2026-02-22 18:54:56 -0500

    remove import service_account

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from telegram.ext import (
 )
 import json
 import gspread
-from google.oauth2.service_account import Credentials, service_account
+from google.oauth2.service_account import Credentials
 
 # ── Logging ───────────────────────────────────────────────────────────────────
 logging.basicConfig(
